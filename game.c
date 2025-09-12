85cf4e3cdc81fa8f614c6f09ac5d2b2086286a8f
Willoh Brubaker <willohbrubaker@outlook.com>
2025-09-12 17:04:04 -0500

    w

--- game.c
+++ game.c
@@ -2,48 +2,58 @@
 #include <stdio.h>
 
 // Rotation angles for the cube
-float rotateX = 0.0f;
-float rotateY = 0.0f;
+float rotateX = 10.0f;
+float rotateY = -130.0f;
 float rotateSpeed = 5.0f; // Degrees per key press
 
-void drawCube() {
+float posX = -9.1f;
+float posY = -6.0f;
+float posZ = -3.6f;
+
+void drawCube(float x, float y, float z, int t) {
+
+    float a = 0.0f;
+
+    if (t) {
+        a = 1.0f - a;
+    }
     glBegin(GL_QUADS);
     // Front face (red)
-    glColor3f(1.0f, 0.0f, 0.0f);
-    glVertex3f(-0.5f, -0.5f, 0.5f);
-    glVertex3f(0.5f, -0.5f, 0.5f);
-    glVertex3f(0.5f, 0.5f, 0.5f);
-    glVertex3f(-0.5f, 0.5f, 0.5f);
+    glColor3f(1.0f, 0.0f, 0.0f + a);
+    glVertex3f(-0.5f + x, -0.5f + y, 0.5f + z);
+    glVertex3f(0.5f + x, -0.5f + y, 0.5f + z);
+    glVertex3f(0.5f + x, 0.5f + y, 0.5f + z);
+    glVertex3f(-0.5f + x, 0.5f + y, 0.5f + z);
     // Back face (green)
     glColor3f(0.0f, 1.0f, 0.0f);
-    glVertex3f(-0.5f, -0.5f, -0.5f);
-    glVertex3f(-0.5f, 0.5f, -0.5f);
-    glVertex3f(0.5f, 0.5f, -0.5f);
-    glVertex3f(0.5f, -0.5f, -0.5f);
+    glVertex3f(-0.5f + x, -0.5f + y, -0.5f + z);
+    glVertex3f(-0.5f + x, 0.5f + y, -0.5f + z);
+    glVertex3f(0.5f + x, 0.5f + y, -0.5f + z);
+    glVertex3f(0.5f + x, -0.5f + y, -0.5f + z);
     // Top face (blue)
     glColor3f(0.0f, 0.0f, 1.0f);
-    glVertex3f(-0.5f, 0.5f, -0.5f);
-    glVertex3f(-0.5f, 0.5f, 0.5f);
-    glVertex3f(0.5f, 0.5f, 0.5f);
-    glVertex3f(0.5f, 0.5f, -0.5f);
+    glVertex3f(-0.5f + x, 0.5f + y, -0.5f + z);
+    glVertex3f(-0.5f + x, 0.5f + y, 0.5f + z);
+    glVertex3f(0.5f + x, 0.5f + y, 0.5f + z);
+    glVertex3f(0.5f + x, 0.5f + y, -0.5f + z);
     // Bottom face (yellow)
     glColor3f(1.0f, 1.0f, 0.0f);
-    glVertex3f(-0.5f, -0.5f, -0.5f);
-    glVertex3f(0.5f, -0.5f, -0.5f);
-    glVertex3f(0.5f, -0.5f, 0.5f);
-    glVertex3f(-0.5f, -0.5f, 0.5f);
+    glVertex3f(-0.5f + x, -0.5f + y, -0.5f + z);
+    glVertex3f(0.5f + x, -0.5f + y, -0.5f + z);
+    glVertex3f(0.5f + x, -0.5f + y, 0.5f + z);
+    glVertex3f(-0.5f + x, -0.5f + y, 0.5f + z);
     // Right face (cyan)
     glColor3f(0.0f, 1.0f, 1.0f);
-    glVertex3f(0.5f, -0.5f, -0.5f);
-    glVertex3f(0.5f, 0.5f, -0.5f);
-    glVertex3f(0.5f, 0.5f, 0.5f);
-    glVertex3f(0.5f, -0.5f, 0.5f);
+    glVertex3f(0.5f + x, -0.5f + y, -0.5f + z);
+    glVertex3f(0.5f + x, 0.5f + y, -0.5f + z);
+    glVertex3f(0.5f + x, 0.5f + y, 0.5f + z);
+    glVertex3f(0.5f + x, -0.5f + y, 0.5f + z);
     // Left face (magenta)
     glColor3f(1.0f, 0.0f, 1.0f);
-    glVertex3f(-0.5f, -0.5f, -0.5f);
-    glVertex3f(-0.5f, -0.5f, 0.5f);
-    glVertex3f(-0.5f, 0.5f, 0.5f);
-    glVertex3f(-0.5f, 0.5f, -0.5f);
+    glVertex3f(-0.5f + x, -0.5f + y, -0.5f + z);
+    glVertex3f(-0.5f + x, -0.5f + y, 0.5f + z);
+    glVertex3f(-0.5f + x, 0.5f + y, 0.5f + z);
+    glVertex3f(-0.5f + x, 0.5f + y, -0.5f + z);
     glEnd();
 }
 
@@ -51,14 +61,26 @@ void display() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
     // Set camera position
-    gluLookAt(0.0f, 0.0f, 2.0f,  // Eye position
-              0.0f, 0.0f, 0.0f,  // Look at origin
+    gluLookAt(posX,posY, posZ,// Eye position
+              posX + 4.0f, posY + 4.0f, posZ + 4.0f,  // Look at origin
               0.0f, 1.0f, 0.0f); // Up vector
     // Apply rotation
     glRotatef(rotateX, 1.0f, 0.0f, 0.0f);
     glRotatef(rotateY, 0.0f, 1.0f, 0.0f);
-    drawCube();
+
+    float z=0.0f;
+    int t=0;
+
+    for (float x=0.0f;x<=8.0f;x+=1.0f) {
+        for (float y=0.0f;y<=8.0f;y+=1.0f){
+            drawCube(x, y, z, t);
+            t = 1 - t;
+            
+        }
+        //t = 1 - t;
+    }
     glutSwapBuffers();
+    
 }
 
 void specialKeys(int key, int x, int y) {
@@ -75,11 +97,39 @@ void specialKeys(int key, int x, int y) {
         case GLUT_KEY_RIGHT: // Rotate right
             rotateY += rotateSpeed;
             break;
+        
     }
     glutPostRedisplay(); // Request redraw
 }
 
 void keyboard(unsigned char key, int x, int y) {
+    if (key == 'w') {
+        posX += 0.1f;
+        display();
+    }
+    if (key == 's') {
+        posX -= 0.1f;
+        display();
+    }
+    if (key == 'a') {
+        posY -= 0.1f;
+        display();
+    }
+    if (key == 'd') {
+        posY += 0.1f;
+        display();
+    }
+    if (key == 'q') {
+        posZ -= 0.1f;
+        display();
+    }
+    if (key == 'e') {
+        posZ += 0.1f;
+        display();
+    }
+    if (key == 'm') {
+        printf("Rotate X: %f\tRotate Y: %f\tX: %f\tY: %f\tZ: %f\n",rotateX,rotateY,posX,posY,posZ);
+    }
     if (key == 27) { // Escape key to exit
         exit(0);
     }
@@ -105,8 +155,8 @@ void reshape(int w, int h) {
 int main(int argc, char** argv) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-    glutInitWindowSize(500, 500);
-    glutCreateWindow("Rotate 3D Cube");
+    glutInitWindowSize(800, 800);
+    glutCreateWindow("Willohlandia");
     init();
     glutDisplayFunc(display);
     glutReshapeFunc(reshape);
